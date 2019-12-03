8f029109eb5ae3a7b0250811323b5dc4bc97a473
ahmedalnemer <ahmedalnemer3@gmail.com>
2019-12-03 14:32:49 +0300

    improved the game further

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 $(function () {
     const images = [`<img src='../images/X.png' > </  img>`, `<img src='../images/O.png' > </  img>`]
     const letter = ['x', 'o']
-    let wins = [0,0]
+    let wins = [0, 0]
     let imageIndex = 0;
     let result = [
         ['n', 'n', 'n'],
@@ -9,39 +9,117 @@ $(function () {
         ['n', 'n', 'n']
     ]
     let gameCount = 0
-    $(".box").hover(function() {
-        $(this).css("background","#2cc0a2")
-      },
-      function(){
-        $(this).css("background","#37e7c4")
-      })
+    let winnerFound = false
+
+    $(".box").hover(function () {
+            $(this).css("background", "#2cc0a2")
+        },
+        function () {
+            $(this).css("background", "#37e7c4")
+        })
 
     const checkWinner = function (column, row) {
+        
         if (result[column][row] != 'n' && result[column][row] == result[(column + 1) % 3][row] && result[column][row] == result[(column + 2) % 3][row]) {
             wins[imageIndex]++
-            $('.'+letter[imageIndex]+'Score').html(''+ wins[imageIndex])
+            $('.' + letter[imageIndex] + 'Score').html('' + wins[imageIndex])
             $('.box').off()
-            $(`h2`).insertAfter(`logo`)
-            $(`h2`).html(`<h2>${letter[imageIndex]} Wins</h2>`)
-
+            $('.box').css('background', '#37e7c4')
+            $(`.logo`).css('marginBottom', '0px')
+            $(`.logo`).after(`<h2> ${letter[imageIndex].toUpperCase()} Wins </h2>`)
+            winnerFound = true
+            for (let i = 0; i < 3; i++) {
+                $(`div[data-row="${row}"] img`).animate({
+                    opacity: '0%'
+                }, {
+                    duration: 300
+                })
+                $(`div[data-row="${row}"] img`).animate({
+                    opacity: '100%'
+                }, {
+                    duration: 300
+                })
+            }
+        
         } else if (result[column][row] != 'n' && result[column][row] == result[column][(row + 1) % 3] && result[column][row] == result[column][(row + 2) % 3]) {
             wins[imageIndex]++
-            $('.'+letter[imageIndex]+'Score').html(''+ wins[imageIndex])
+            $('.' + letter[imageIndex] + 'Score').html('' + wins[imageIndex])
             $('.box').off()
-            $(`h2`).insertAfter(`logo`)
-        }
-        if (column == row || Math.abs(column - row) == 2) {
+            $('.box').css('background', '#37e7c4')
+            $(`.logo`).css('marginBottom', '0px')
+            $(`.logo`).after(`<h2> ${letter[imageIndex].toUpperCase()} Wins </h2>`)
+            winnerFound = true
+            for (let i = 0; i < 3; i++) {
+                $(`div[data-column="${column}"] img`).animate({
+                    opacity: '0%'
+                }, {
+                    duration: 300
+                })
+                $(`div[data-column="${column}"] img`).animate({
+                    opacity: '100%'
+                }, {
+                    duration: 300
+                })
+            }
+        } else if (column == row || Math.abs(column - row) == 2) {
             if (result[0][0] != 'n' && result[0][0] == result[1][1] && result[0][0] == result[2][2]) {
                 wins[imageIndex]++
-                $('.'+letter[imageIndex]+'Score').html(''+ wins[imageIndex])
+                $('.' + letter[imageIndex] + 'Score').html('' + wins[imageIndex])
                 $('.box').off()
-                $(`h2`).insertAfter(`logo`)
+                $('.box').css('background', '#37e7c4')
+                $(`.logo`).css('marginBottom', '0px')
+                $(`.logo`).after(`<h2> ${letter[imageIndex].toUpperCase()} Wins </h2>`)
+                winnerFound = true
+                for (let i = 0; i < 3; i++) {
+                    $(`div[data-column="0"][data-row="0"] img,
+                    div[data-column="1"][data-row="1"] img,
+                    div[data-column="2"][data-row="2"] img`).animate({
+                        opacity: '0%'
+                    }, {
+                        duration: 300
+                    })
+                    $(`div[data-column="0"][data-row="0"] img,
+                    div[data-column="1"][data-row="1"] img,
+                    div[data-column="2"][data-row="2"] img`).animate({
+                        opacity: '100%'
+                    }, {
+                        duration: 300
+                    })
+                }
+
             } else if (result[0][2] != 'n' && result[0][2] == result[1][1] && result[0][2] == result[2][0]) {
                 wins[imageIndex]++
-                $('.'+letter[imageIndex]+'Score').html(''+ wins[imageIndex])
+                $('.' + letter[imageIndex] + 'Score').html('' + wins[imageIndex])
                 $('.box').off()
-                $(`h2`).insertAfter(`logo`)
+                $('.box').css('background', '#37e7c4')
+                $(`.logo`).css('marginBottom', '0px')
+                $(`.logo`).after(`<h2> ${letter[imageIndex].toUpperCase()} Wins </h2>`)
+                winnerFound = true
+                for (let i = 0; i < 3; i++) {
+                    $(`div[data-column="0"][data-row="2"] img,
+                    div[data-column="1"][data-row="1"] img,
+                    div[data-column="2"][data-row="0"] img`).animate({
+                        opacity: '0%'
+                    }, {
+                        duration: 300
+                    })
+                    $(`div[data-column="0"][data-row="2"] img,
+                    div[data-column="1"][data-row="1"] img,
+                    div[data-column="2"][data-row="0"] img`).animate({
+                        opacity: '100%'
+                    }, {
+                        duration: 300
+                    })
+                }
+
             }
+        }
+
+        console.log(gameCount)
+        console.log(winnerFound)
+        if (gameCount == 9 && !winnerFound) {
+            $(`.logo`).css('marginBottom', '0px')
+            $(`.logo`).after(`<h2> It's a Tie </h2>`)
         }
     }
 
@@ -49,10 +127,10 @@ $(function () {
         $(this).html(images[imageIndex])
         result[$(this).attr('data-column')][$(this).attr('data-row')] = letter[imageIndex]
         $(this).off()
-        $(this).css('background','#37e7c4')
+        $(this).css('background', '#37e7c4')
         gameCount++
-        if (gameCount > 4){
-        checkWinner($(this).attr('data-column'), $(this).attr('data-row'));
+        if (gameCount > 4) {
+            checkWinner($(this).attr('data-column'), $(this).attr('data-row'));
         }
         imageIndex = (imageIndex + 1) % 2
     }
@@ -68,20 +146,24 @@ $(function () {
             ['n', 'n', 'n']
         ]
         gameCount = 0
-        $(".box").hover(function() {
-            $(this).css("background","#2cc0a2")
-          },
-          function(){
-            $(this).css("background","#37e7c4")
-          })
+        $(".box").hover(function () {
+                $(this).css("background", "#2cc0a2")
+            },
+            function () {
+                $(this).css("background", "#37e7c4")
+            })
+
+        $(`.logo`).css('marginBottom', '73px')
+        $('h2').remove()
+        winnerFound = false
     }
 
-    const newGame = function(){
+    const newGame = function () {
         location.reload()
     }
 
     $('.box').on('click', addImage)
     $('.reset').on('click', reset)
-    $('.newGame').on('click',newGame)
+    $('.newGame').on('click', newGame)
 
 })
